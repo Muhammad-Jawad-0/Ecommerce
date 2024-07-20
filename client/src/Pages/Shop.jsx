375726880a0a7aba606016077e90ff329d33547d
Muhammad Jawad <muhammadjawadforgithub@gmail.com>
2024-07-21 02:57:12 +0500

    some components added

--- client/src/Pages/Shop.jsx
+++ client/src/Pages/Shop.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Hero from '../Components/Hero/Hero'
+import Polular from '../Components/Popular/Polular'
+import Offers from '../Components/Offers/Offers'
 
 const Shop = () => {
   return (
     <div>
       <Hero />
+      <Polular />
+      <Offers />
     </div>
   )
 }
