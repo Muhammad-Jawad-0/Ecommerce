e128763611ca7b26f1cadae0a9ea3d516702eba0
Muhammad Jawad <muhammadjawadforgithub@gmail.com>
2024-07-23 18:42:24 +0500

    working is on the way

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,14 @@ import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Shop from "./Pages/Shop";
-import ShopCatogory from "./Pages/ShopCatogory";
+import ShopCatogory from "./Pages/ShopCategory";
 import Product from "./Pages/Product";
 import Cart from "./Pages/Cart";
 import LoginSignup from "./Pages/LoginSignup";
+import Footer from "./Components/Footer/Footer";
+import men_banner from "./Components/Assets/banner_mens.png"
+import women_banner from "./Components/Assets/banner_women.png"
+import kid_banner from "./Components/Assets/banner_kids.png"
 function App() {
   return (
     <div>
@@ -13,15 +17,16 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Shop />} />
-          <Route path="/mens" element={<ShopCatogory category="men" />} />
-          <Route path="/womens" element={<ShopCatogory category="women" />} />
-          <Route path="/kids" element={<ShopCatogory category="kid" />} />
+          <Route path="/mens" element={<ShopCatogory banner={men_banner} category="men" />} />
+          <Route path="/womens" element={<ShopCatogory banner={women_banner} category="women" />} />
+          <Route path="/kids" element={<ShopCatogory banner={kid_banner} category="kid" />} />
           <Route path="/product" element={<Product />}>
             <Route path=":productId" element={<Product />} />
           </Route>
           <Route path="/cart" element={<Cart />} />
           <Route path="/login" element={<LoginSignup />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
--- client/src/Components/NewsLetter/NewsLetter.jsx
+++ client/src/Components/NewsLetter/NewsLetter.jsx
@@ -8,6 +8,7 @@ const NewsLetter = () => {
       <p>Subscribe to our newsletter and stay updated</p>
       <div>
         <input type="email" placeholder='Your Email id' />
+        <button>Subscribe</button>
       </div>
     </div>
   )
