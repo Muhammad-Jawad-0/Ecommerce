6082386c546dc32bd111530935d2309c114f8566
Muhammad Jawad <muhammadjawadforgithub@gmail.com>
2024-07-23 20:00:00 +0500

    Added DescriptionBox

---
+++ client/src/Components/DiscriptionBox/DiscriptionBox.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./DiscriptionBox.css";
+
+const DiscriptionBox = () => {
+  return (
+    <div className="descriptionbox">
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur quos odio fugit nobis numquam sequi, adipisci ab explicabo iusto delectus quam? Adipisci explicabo quo id facilis exercitationem, quidem nulla minus natus saepe molestiae minima, at ullam quos beatae aspernatur cum laborum consequuntur necessitatibus? Iure dicta neque voluptas quaerat magnam ad dolorum obcaecati laudantium placeat quia aut, voluptate autem error sequi voluptatibus dolor itaque temporibus ea exercitationem assumenda. Consectetur praesentium nisi minus consequatur modi exercitationem magnam, ipsa eum similique sunt optio neque qui eligendi quae cumque tenetur dolore aliquid sequi veritatis? Exercitationem nihil fugit labore nulla ex tempore esse doloremque in!
+        </p>
+
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa perspiciatis dolor, incidunt voluptates fuga eligendi non velit eveniet quo maiores aperiam quas esse quos facilis magnam aliquam modi, quae libero!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default DiscriptionBox;
